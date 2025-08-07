73164325c0e051fca4e08ce40abb708e9c4efe39
Muhammad Nasir <92ai.tech@gmail.com>
2025-08-07 16:39:47 +0500

    Delete config directory

--- config/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY", "your-api-key-here")
-
-BASE_VOICE_ID = "TxGEqnHWrfWFTfGW9XjX"  # Example ElevenLabs voice ID
-
-ASSETS_DIR = "data/sfx"
-OUTPUT_DIR = "outputs/chapters"
